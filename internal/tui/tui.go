a88e6ea3b589e528dbc0fc41e1416df9d7f16cb4
Steffen Brunner <steffenbrunner@gmail.com>
2025-08-18 17:20:31 +0200

    feat: initial terminal ui enhancement with --crd / --kind

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -17,15 +17,40 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package tui
 
 import (
+	"context"
 	tea "github.com/charmbracelet/bubbletea"
-
 	"github.com/pehlicd/crd-wizard/internal/k8s"
+	"github.com/pehlicd/crd-wizard/internal/models"
 )
 
 // Start initializes and runs the Bubble Tea TUI.
-func Start(client *k8s.Client) error {
+func Start(client *k8s.Client, crdName string, kind string) error {
 	mainModel := newMainModel(client)
 	p := tea.NewProgram(mainModel, tea.WithAltScreen(), tea.WithMouseCellMotion())
+
+	if kind != "" || crdName != "" {
+		var targetCRD models.CRD
+		var found bool
+
+		allCRDs, err := client.GetCRDs(context.Background())
+		if err != nil {
+			return err
+		}
+
+		for _, crd := range allCRDs {
+			if (crdName != "" && crd.Name == crdName) || (kind != "" && crd.Kind == kind) {
+				targetCRD = crd
+				found = true
+				break
+			}
+		}
+		if found {
+			// Set the view to instanceListView and initialize the corresponding model.
+			instanceModel := newInstanceListModelWithActiveTab(client, targetCRD, mainModel.height, mainModel.width, schemaTab)
+			p = tea.NewProgram(instanceModel, tea.WithAltScreen(), tea.WithMouseCellMotion())
+		}
+	}
+
 	_, err := p.Run()
 	return err
 }
