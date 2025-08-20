3b48f38c361390b88e8fa143a1509b096a1c9640
pehlicd <furkanpehlivan34@gmail.com>
2025-08-20 21:49:35 +0200

    chore: rename project
    Signed-off-by: pehlicd <furkanpehlivan34@gmail.com>

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -9,18 +9,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    unoptimized: true,
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
 };
 
 export default nextConfig;
--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -19,6 +19,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+          <link rel="icon" href="/favicon.ico" />
+          <title>CR(D) Explorer</title>
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
